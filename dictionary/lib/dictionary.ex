7143d5fed3217fae621daa6f60857c57e1f799c4
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-03-08 11:44:50 -0800

    docs: added basic docstrings (mostly syntax testing)
    Note: also discovered that `r <modulename>` in iex does not seem to
    compile those doc strings and/or `h <...>` does not register them

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,6 +1,23 @@
 defmodule Dictionary do
-  def wordlist do
+  @moduledoc """
+  Dictionary module
+  """
+
+  @doc """
+  Exploring syntax: pattern matching & piping
+  Not a useful component of the larger program
+  """
+  def wordlistprint do
     {:ok, words} = File.read("assets/words.txt")
     String.split(words) |> IO.puts
+  end
+
+  @doc """
+  returns a list of words -- cannonical word list for now
+  assumes sourcing from a specific file in relative path
+  """
+  def word_list do
+    {:ok, words} = File.read("assets/words.txt")
+    String.split(words)
   end
 end
