6bf5def9c8d3fadcde5f55577e18e8e76f7d3e7e
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-03-11 15:02:47 -0800

    refact: typed Game struct

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,6 +1,12 @@
 defmodule Hangman.Impl.Game do
 
   # this struct is connected to module and called via module's name
+  @type t :: %Hangman.Impl.Game{
+    turns_left: integer,
+    game_state: Hangman.state,
+    letters: list(String.t),
+    used: MapSet.t(String.t)
+  }
   defstruct(
     turns_left: 7,
     game_state: :initializing,
