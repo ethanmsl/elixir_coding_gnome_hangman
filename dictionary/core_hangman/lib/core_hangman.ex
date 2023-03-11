694a6dfa74110635561741f395927d9d96011c74
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-03-11 14:01:06 -0800

    refactor: added typing (provisional)

--- dictionary/core_hangman/lib/core_hangman.ex
+++ dictionary/core_hangman/lib/core_hangman.ex
@@ -1,4 +1,13 @@
 defmodule CoreHangman do
+  @type state:: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
+  @type game :: any
+  @type tally :: %:
+    turns_left: integer,
+    game_state: state,
+    letters: list(String.t),
+    used: list(String.t)
+}
+
   @moduledoc """
   Core logic to our "hangman" game.
   (dark theme!)
@@ -7,12 +16,14 @@ defmodule CoreHangman do
   @doc """
   instantiates ... a new game
   """
+  @spec new_game :: game
   def new_game do
   end
 
   @doc """
   takes input from player and relays to game representation
   """
+    @spec make_guess(game, String.t) :: { game, tally }
   def play_game(game, guess) do
   end
 end
