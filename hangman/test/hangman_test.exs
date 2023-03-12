d4ab863c3af0edaa8c00467d106c213b0856dc08
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-03-11 17:26:34 -0800

    explr: example test

--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -2,7 +2,7 @@ defmodule HangmanTest do
   use ExUnit.Case
   doctest Hangman
 
-  # test "greets the world" do
-  #   assert CoreHangman.hello() == :world
-  # end
+  test "reality" do
+    assert 1 + 1 == 2
+  end
 end
