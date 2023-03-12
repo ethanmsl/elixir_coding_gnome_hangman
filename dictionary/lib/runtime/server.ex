d31ef0da07242313aa14c808951612e0b87d2b27
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-03-12 15:24:58 -0700

    feat: application-process auto runnign to grab and hold dictionary state

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -1,14 +1,17 @@
 defmodule Dictionary.Runtime.Server do
 
   @type t :: pid()
-alias Dictionary.Impl.WordList
+  alias Dictionary.Impl.WordList
 
-def start_link do
-  Agent.start_link(&WordList.word_list/0)
-  # returns {:ok/:error, pid}
-end
+  def start_link do
+    # `name: <atom>` registers pid as <atom>
+    Agent.start_link(&WordList.word_list/0, name: :wilma)
+    # returns {:ok/:error, pid}
+  end
 
-def random_word(pid) do
-  Agent.get(pid, &WordList.random_word/1)
-end
+  def random_word() do
+    # **strong** coupling of `random_word()` to `start_link()`
+    # by way of hard-coded (dynamic) pid reference
+    Agent.get(:wilma, &WordList.random_word/1)
+  end
 end
