e5ed9c7e9fa094880ee8323d4bd38a9d62f8ab6c
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-03-12 15:46:02 -0700

    refact: better process naming

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -3,15 +3,17 @@ defmodule Dictionary.Runtime.Server do
   @type t :: pid()
   alias Dictionary.Impl.WordList
 
+  @me __MODULE__
+
   def start_link do
     # `name: <atom>` registers pid as <atom>
-    Agent.start_link(&WordList.word_list/0, name: :wilma)
+    Agent.start_link(&WordList.word_list/0, name: @me)
     # returns {:ok/:error, pid}
   end
 
   def random_word() do
     # **strong** coupling of `random_word()` to `start_link()`
     # by way of hard-coded (dynamic) pid reference
-    Agent.get(:wilma, &WordList.random_word/1)
+    Agent.get(@me, &WordList.random_word/1)
   end
 end
