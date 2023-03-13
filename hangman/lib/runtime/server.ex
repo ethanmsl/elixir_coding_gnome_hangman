5c9ad9ab7dee1078cb252f62173392f81a435323
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-03-12 18:06:07 -0700

    feat: calls for GenServer

--- hangman/lib/runtime/server.ex
+++ hangman/lib/runtime/server.ex
@@ -12,4 +12,13 @@ defmodule Hangman.Runtime.Server do
   def init(_) do
     {:ok, Game.new_game}
   end
+  
+  def handle_call({:make_move, guess}, _from, game) do
+    {update_game, tally} = Game.make_move(game, guess)
+    {:reply, tally, update_game}
+  end
+
+  def handle_call({:tally}, _from,  game) do
+    {:reply, Game.tally(game), game}
+  end
 end
